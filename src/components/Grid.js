7fef30797f4bddda87e44d21d8529bc67b40a645
ArturDeAndrade <adeandrade.42@gmail.com>
2020-06-30 11:36:55 -0300

    Creating the main components(the grid and its nine cells).

---
+++ src/components/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Cell from './Cell'
+
+const Grid = () => {
+    return (
+        <div>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+    )
+}
+
+export default Grid
